8fc3bde01455da63bbe2957c36010650584e0f3b
Pedro Bellas <pedro.bellas12@gmail.com>
2020-07-26 21:26:55 -0300

    Correção de erros: Elemento 4

--- JS/ELEMENTO4/script.js
+++ JS/ELEMENTO4/script.js
@@ -1,10 +1,32 @@
-/*Pegando o elemento formulario*/
-const f1 = document.forms;
-const f2 = document.forms[0];
-const f3 = document.getElementsByName('pesquisa');
-/*Usando o name no HTML para pegar elementos*/
-const ff = document.pesquisa;
-console.log(ff);
-/*Modificando valores dos campos do formulario*/
-document.pesquisa.q.value = 'Pedro';
-document.pesquisa.hl.value = "en-us";
+/*Só pra demonstrar a flexibilidade do Javascript*/
+function log () {
+	console.log(this);
+}
+
+/*Todos esses elementos agora se chamar a função log será exibido no console*/
+HTMLCollection.prototype.log = log;
+HTMLElement.prototype.log = log;
+NodeList.prototype.log = log;
+Node.prototype.log = log;
+
+
+const lista = document.querySelector('div ul');
+/*Selecionando o pai da lista*/
+lista.parentNode.log();
+/*Selecionando os 3 lis da lista*/
+lista.childNodes.log();
+/*os espaços são elementos de textos igual acontece ao display: inline-block;*/
+
+/*Selecionando o primeiro item da lista*/
+const primeiro = lista.firstChild;
+primeiro.log(); /*O texto espaço de texto em branco*/
+primeiro.nextSibling.log(); /*´pega o Pedro*/
+
+const ultimo = lista.lastChild;
+ultimo.log(); /*O texto espaço de texto em branco*/
+ultimo.previousSibling.log(); /*Pega a Bia*/
+
+/*Pega os filhos mas sem o espaço em branco*/
+lista.children.log();
+lista.firstElementChild.log();
+lista.lastElementChild.log();
