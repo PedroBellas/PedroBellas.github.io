2d8e90c0a629a2477af28bb695e21359e5103522
Pedro Bellas <pedro.bellas12@gmail.com>
2020-07-30 08:58:37 -0300

    Menu do index finalizado

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,15 @@
 const botao = document.querySelector('#hamburguer');
+const menu = document.querySelector('#menu');
+
 
 botao.onclick = function(e){
 
 	if(botao.classList[1] == 'active-btn'){
+		menu.classList.remove('active-menu');
 		botao.classList.remove('active-btn');
 	}
 	else{
+		menu.classList.add('active-menu');
 		botao.classList.add('active-btn');	
 	}
 
