21d03e210c9c3e8e9b98b42a4ee78d3e3317b1ef
Pedro Bellas <pedro.bellas12@gmail.com>
2020-07-30 12:30:00 -0300

    Scroll bar e banner (loading e finilizado)

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,23 @@
 const botao = document.querySelector('#hamburguer');
 const menu = document.querySelector('#menu');
+const bar = document.querySelector('#progress-bar');
+const lBox = document.querySelector('#loading');
+const ulBall = document.querySelector("ul.ball");
+const corpo = document.querySelector("body");
 
+for(let i = 0; i < 15; i++){
+
+	const li = document.createElement("li");
+	ulBall.appendChild(li);
+
+	const position = Math.random() * (90 - 1) + 1;
+	const time = Math.random() * (5 - 0.1) + 0.1;
+	const start = Math.random() * (20 - 10) + 10;
+
+	li.style.left = position+"%";
+	li.style.animationDelay = time+"s";
+	li.style.animationDuration = start+"s";
+}
 
 botao.onclick = function(e){
 
@@ -14,3 +31,16 @@ botao.onclick = function(e){
 	}
 
 }
+
+function load() {
+	lBox.classList.add('hidden');
+}
+
+corpo.onload = function(e){
+
+	bar.classList.add('full');
+
+	setTimeout(load, 5200);
+
+}
+
