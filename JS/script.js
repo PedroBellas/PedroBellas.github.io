6baa79462e682db7e9cf42d3beaf8fb3b9ebb289
Pedro Bellas <pedro.bellas12@gmail.com>
2020-10-28 16:08:26 -0300

    Adicionando a respansividade para os sliders

--- JS/script.js
+++ JS/script.js
@@ -11,8 +11,6 @@ const lBox = document.querySelector('#loading');
 const ulBall = document.querySelector("ul.ball");
 const corpo = document.querySelector("body");
 
-
-
 const area1 = document.querySelector('#area1');
 const area2 = document.querySelector('#area2');
 const area3 = document.querySelector('#area3');
@@ -39,6 +37,165 @@ const js1 = document.querySelector('#areaJs1');
 const js2 = document.querySelector('#areaJs2');
 const anteriorJs = document.querySelector('#anteriorJs');
 const proximoJs = document.querySelector('#proximoJs');
+
+const menuItens = document.querySelectorAll('.menu a[href^="#"]');
+
+menuItens.forEach(item => {
+	item.addEventListener('click', scrollSuave);
+});
+
+function scrollSuave(event){
+
+	event.preventDefault();
+
+	const valorMenu = event.target.getAttribute('href');
+	const secao = document.querySelector(valorMenu);
+
+	window.scroll({
+
+		/*560 = tamanho do banner principal*/
+		/*80 =  o menu fixo*/
+
+		top: secao.offsetTop + 560 - 80,
+		behavior: "smooth",
+	});
+
+}
+
+
+/*
+===========================
+Animação das bolinhas
+===========================
+*/
+
+for(let i = 0; i < 15; i++){
+
+	const li = document.createElement("li");
+	ulBall.appendChild(li);
+
+	const position = Math.random() * (75) + 1;
+	const time = Math.random() * (5 - 0.1) + 0.1;
+	const start = Math.random() * (20 - 10) + 10;
+
+	li.style.left = position+"%";
+	li.style.animationDelay = time+"s";
+	li.style.animationDuration = start+"s";
+}
+
+/*
+===========================
+Animação do botão
+===========================
+*/
+
+botao.onclick = function(){
+
+	if(botao.classList[1] == 'active-btn'){
+		menu.classList.remove('active-menu');
+		botao.classList.remove('active-btn');
+	}
+	else{
+		menu.classList.add('active-menu');
+		botao.classList.add('active-btn');	
+	}
+
+}
+
+/*
+===========================
+Animação do load
+===========================
+*/
+
+function load() {
+	lBox.classList.add('hidden');
+}
+
+corpo.onload = function(){
+
+	bar.classList.add('full');
+
+	setTimeout(load, 5200);
+
+}
+
+function show(e) {
+	e.classList.add('slow-show');
+}
+
+function hidd(e){
+	e.classList.remove('slow-show');	
+}
+
+/*
+===========================
+Animação de scroll 
+===========================
+*/
+
+window.onscroll = function(e){
+
+	/*Pega o ponto de cima
+	/*Pegando a posição de um elemento
+	/*Fazendo a comparação*/
+
+	if(window.scrollY > area1.getBoundingClientRect().y){
+		show(area1);
+
+		if (window.scrollY > area2.getBoundingClientRect().y + area1.getBoundingClientRect().height) {
+			show(area2);
+
+			if (window.scrollY > area3.getBoundingClientRect().y + area2.getBoundingClientRect().height + area3.getBoundingClientRect().height) {
+				show(area3);
+			}
+			else{
+				hidd(area3);
+			}
+		}
+		else{
+			hidd(area2);
+		}
+	}
+	else{
+		hidd(area1);
+	}
+
+}
+
+/*
+===========================
+Animação da bolinha correndo
+===========================
+*/
+
+
+function die(){
+	runningBall.style.display = "none";
+}
+
+runningBall.onclick = function(){
+	const ballSon = document.querySelectorAll('#run-ball span');
+
+	var number = ballSon.length;
+	
+	if(number == 7){
+
+		runningBall.classList.add('die-ball');
+		setTimeout(die, 4000);
+
+	}
+	else{
+
+		const span = document.createElement("span");
+		runningBall.appendChild(span);
+
+		var value = (number + 1) * 5;
+
+		span.style.filter = 'blur('+value+'px)';
+
+	}
+}
 
 /*
 ===========================
@@ -204,138 +361,4 @@ proximoJs.onclick = function(){
 		valorJs = 1;	
 	}
 
-}
-
-/*
-===========================
-Animação das bolinhas
-===========================
-*/
-
-for(let i = 0; i < 15; i++){
-
-	const li = document.createElement("li");
-	ulBall.appendChild(li);
-
-	const position = Math.random() * (75) + 1;
-	const time = Math.random() * (5 - 0.1) + 0.1;
-	const start = Math.random() * (20 - 10) + 10;
-
-	li.style.left = position+"%";
-	li.style.animationDelay = time+"s";
-	li.style.animationDuration = start+"s";
-}
-
-/*
-===========================
-Animação do botão
-===========================
-*/
-
-botao.onclick = function(){
-
-	if(botao.classList[1] == 'active-btn'){
-		menu.classList.remove('active-menu');
-		botao.classList.remove('active-btn');
-	}
-	else{
-		menu.classList.add('active-menu');
-		botao.classList.add('active-btn');	
-	}
-
-}
-
-/*
-===========================
-Animação do load
-===========================
-*/
-
-function load() {
-	lBox.classList.add('hidden');
-}
-
-corpo.onload = function(){
-
-	bar.classList.add('full');
-
-	setTimeout(load, 5200);
-
-}
-
-function show(e) {
-	e.classList.add('slow-show');
-}
-
-function hidd(e){
-	e.classList.remove('slow-show');	
-}
-
-/*
-===========================
-Animação de scroll 
-===========================
-*/
-
-window.onscroll = function(e){
-
-	/*Pega o ponto de cima
-	/*Pegando a posição de um elemento
-	/*Fazendo a comparação*/
-
-	if(window.scrollY > area1.getBoundingClientRect().y){
-		show(area1);
-
-		if (window.scrollY > area2.getBoundingClientRect().y + area1.getBoundingClientRect().height) {
-			show(area2);
-
-			if (window.scrollY > area3.getBoundingClientRect().y + area2.getBoundingClientRect().height + area3.getBoundingClientRect().height) {
-				show(area3);
-			}
-			else{
-				hidd(area3);
-			}
-		}
-		else{
-			hidd(area2);
-		}
-	}
-	else{
-		hidd(area1);
-	}
-
-}
-
-/*
-===========================
-Animação da bolinha correndo
-===========================
-*/
-
-
-function die(){
-	runningBall.style.display = "none";
-}
-
-runningBall.onclick = function(){
-	const ballSon = document.querySelectorAll('#run-ball span');
-
-	var number = ballSon.length;
-	
-	if(number == 7){
-
-		runningBall.classList.add('die-ball');
-		setTimeout(die, 4000);
-
-	}
-	else{
-
-		const span = document.createElement("span");
-		runningBall.appendChild(span);
-
-		var value = (number + 1) * 5;
-
-		span.style.filter = 'blur('+value+'px)';
-
-	}
 }
